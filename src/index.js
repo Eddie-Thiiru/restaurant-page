7b6007c54165eb709b72d63f168b49648c49075c
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-04-19 10:06:58 +0300

    Add webpack.config.js

--- src/index.js
+++ src/index.js
@@ -1,20 +1,6 @@
-import Img from "./restaurant.jpg";
+import { initialPage } from "./initial.js";
 
-const initialPage = () => {
-  const container = document.querySelector(".content");
-  const heading = document.createElement("h1");
-  const para = document.createElement("P");
-  const img = new Image();
-
-  img.src = Img;
-  img.alt = "Restaurant image";
-  heading.textContent = "Tasty Good Kitchen";
-  para.textContent =
-    "Tasty Good Kitchen is proud to introduce Austin's premier Josper oven experience. Our menu showcases live-fire cooking with Latin flare. Subterra Agave Bar is our dimly-lit cocktail lounge, located downstairs from Ember. It features craft Agave cocktails and a wide selection of Latin spirits.";
-
-  container.appendChild(heading);
-  container.appendChild(img);
-  container.appendChild(para);
-};
-
-export { initialPage };
+function component() {
+  initialPage();
+}
+component();
--- src/initial.js
+++ src/initial.js
@@ -1,6 +1,20 @@
-import { initialPage } from "./initial.js";
+import Img from "./restaurant.jpg";
 
-function component() {
-  initialPage();
-}
-component();
+const initialPage = () => {
+  const container = document.querySelector(".content");
+  const heading = document.createElement("h1");
+  const para = document.createElement("P");
+  const img = new Image();
+
+  img.src = Img;
+  img.alt = "Restaurant image";
+  heading.textContent = "Tasty Good Kitchen";
+  para.textContent =
+    "Tasty Good Kitchen is proud to introduce Austin's premier Josper oven experience. Our menu showcases live-fire cooking with Latin flare. Subterra Agave Bar is our dimly-lit cocktail lounge, located downstairs from Ember. It features craft Agave cocktails and a wide selection of Latin spirits.";
+
+  container.appendChild(heading);
+  container.appendChild(img);
+  container.appendChild(para);
+};
+
+export { initialPage };
