c43d26f169488981aee41623c0566c359ce1944c
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-04-20 11:46:33 +0300

    Add home page image

--- src/initial.js
+++ src/initial.js
@@ -1,6 +1,6 @@
 import { menuBtn } from "./menu";
 import { contactBtn } from "./contact";
-import Img from "./restaurant.jpg";
+import Img from "./images/grill.jpg";
 import "./styles.css";
 
 const container = document.querySelector(".content");
@@ -35,6 +35,7 @@ const homePage = () => {
   mainSection.classList.add("main-section");
   para.textContent =
     "Tasty Good Kitchen is proud to introduce Austin's premier Josper oven experience. Our menu showcases live-fire cooking with Latin flare. Subterra Agave Bar is our dimly-lit cocktail lounge, located downstairs from Ember. It features craft Agave cocktails and a wide selection of Latin spirits.";
+  img.classList.add("home-image");
   img.src = Img;
   img.alt = "Restaurant image";
 
