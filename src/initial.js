7fee1e8f4b829c903c77c57d6aaf122294f09f0e
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-04-20 12:43:11 +0300

    Add menu images

--- src/initial.js
+++ src/initial.js
@@ -37,7 +37,7 @@ const homePage = () => {
     "Tasty Good Kitchen is proud to introduce Austin's premier Josper oven experience. Our menu showcases live-fire cooking with Latin flare. Subterra Agave Bar is our dimly-lit cocktail lounge, located downstairs from Ember. It features craft Agave cocktails and a wide selection of Latin spirits.";
   img.classList.add("home-image");
   img.src = Img;
-  img.alt = "Restaurant image";
+  img.alt = "A man cooking meat on a fiery grill";
 
   mainSection.appendChild(img);
   mainSection.appendChild(para);
--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,9 @@
+import Img from "./images/assorted-meats.jpg";
+import Img2 from "./images/ribs.jpg";
+import Img3 from "./images/steak-puree.jpg";
+import Img4 from "./images/pork.jpg";
+import Img5 from "./images/cheeseburger.jpg";
+import Img6 from "./images/fish.jpg";
 import "./styles.css";
 
 const menuBtn = () => {
@@ -11,6 +17,34 @@ const menuBtn = () => {
   nav.appendChild(btn);
 };
 
-const menuPage = () => {};
+const menuPage = () => {
+  const assorted = new Image();
+  const ribs = new Image();
+  const steak = new Image();
+  const pork = new Image();
+  const burger = new Image();
+  const fish = new Image();
+
+  assorted.classList.add("assorted-meats");
+  ribs.classList.add("ribs");
+  steak.classList.add("steak");
+  pork.classList.add("pork");
+  burger.classList.add("burger");
+  fish.classList.add("fish");
+
+  assorted.src = Img;
+  ribs.src = Img2;
+  steak.src = Img3;
+  pork.src = Img4;
+  burger.src = Img5;
+  fish.src = Img6;
+
+  assorted.alt = "Assorted meats on a plate";
+  ribs.alt = "Grilled ribs";
+  steak.alt = "Steak and potato puree on a plate";
+  pork.alt = "A pork steak on a plate";
+  burger.alt = "A double patty cheeseburger";
+  fish.alt = "Roasted fish and vegetables on a plate";
+};
 
 export { menuBtn, menuPage };
